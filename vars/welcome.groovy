0b232a21dc603518c5f88b0ce3bb890f91e626f6
sateeshangadi24 <103989555+sateeshangadi24@users.noreply.github.com>
2022-05-31 11:30:36 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -2,12 +2,5 @@ def call(name){
   echo"Hey ${name},How are you???"
 }
 def cal(num){
-  if(num>0){
-    for(int i=0;i<num;i++){
-      echo"iam printing ${num}"
-    }
-  }
-  else{
-     echo"the number is less than 0"
-  }
+  echo"the number is ${num}"
 }

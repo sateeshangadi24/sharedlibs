7ec65f4e1e2defb8bc071945c52d6dfcd8f07cb6
sateeshangadi24 <103989555+sateeshangadi24@users.noreply.github.com>
2022-05-31 12:20:32 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,6 +1,6 @@
 def call(name,adress){
-  echo"Hey ${name},How are you???"
+  echo"Hey ${name,adress},How are you???"
 }
 def get(num){
-  echo"the number is ${num} and ${no}"
+  echo"the number is ${num} "
 }
